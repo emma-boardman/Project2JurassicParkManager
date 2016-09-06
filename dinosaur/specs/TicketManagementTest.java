d0fa2f241ccba328ac8365f11d4084f3e790b24c
Emma Boardman <emma@emmaboardman.co.uk>
2016-09-06 13:09:26 +0100

    ticket creation factory tests passing

--- dinosaur/specs/TicketManagementTest.java
+++ dinosaur/specs/TicketManagementTest.java
@@ -7,10 +7,28 @@ public class TicketManagementTest {
 
 
     @Test
-    public void canCreateNewTicket(){
+    public void canCreateAdultTicket(){
       TicketFactory ticketFactory = new TicketFactory();
       Ticket ticket = ticketFactory.createTicket("Adult");
       assertEquals("Ticket for visitors aged between 13 and 60", ticket.getDescription());
+
+    }
+
+
+    @Test
+    public void canCreateKidsTicket(){
+      TicketFactory ticketFactory = new TicketFactory();
+      Ticket ticket = ticketFactory.createTicket("Kids");
+      assertEquals("For visitors 12 and under", ticket.getDescription());
+
+    }
+
+
+    @Test
+    public void canCreateConcessionTicket(){
+      TicketFactory ticketFactory = new TicketFactory();
+      Ticket ticket = ticketFactory.createTicket("Conc.");
+      assertEquals("Discounted price for NUS card holders and Senior Citizens (60 and over)", ticket.getDescription());
 
     }
 
